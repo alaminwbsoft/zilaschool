2715284a5835e80ee55be5749a4271156f5310cd
alaminwbsoft <wbsoft.alamin@gmail.com>
2024-12-27 11:11:40 +0600

    add

---
+++ src/Pages/BreakingNotice/BreakingNotice.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+const BreakingNotice = () => {
+  return (
+    <>
+      <div>
+        <Marquee>
+          I can be a React component, multiple React components, or just some
+          text.
+        </Marquee>
+      </div>
+    </>
+  );
+};
+
+export default BreakingNotice;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import NoticeBoard from './Notice/Noticeboard';
-import Subornojoyonti from './Subornojoyonti/Subornojoyonti';
-import Downloads from './DownLoads/Downloads';
+import NoticeBoard from "./Notice/Noticeboard";
+import Subornojoyonti from "./Subornojoyonti/Subornojoyonti";
+import Downloads from "./DownLoads/Downloads";
+import BreakingNotice from "./BreakingNotice/BreakingNotice";
 
 const Home = () => {
-    return (
-        <div>
-            <Subornojoyonti/>
-           <NoticeBoard/>
-           <Downloads/>
-        </div>
-    );
+  return (
+    <div>
+      <Subornojoyonti />
+      <NoticeBoard />
+      <BreakingNotice />
+      <Downloads />
+    </div>
+  );
 };
 
 export default Home;
